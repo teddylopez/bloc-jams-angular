69b9ddcc633e216d9e3f48006fefb1f66b27f555
Lopez-Barnard <LopezE@advisory.com>
2017-03-11 12:29:43 -0500

    code cleanup

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -9,17 +9,14 @@
         $stateProvider
             .state('landing', {
                 url: '/',
-                controller: 'LandingCtrl as landing',
                 templateUrl: '/templates/landing.html'
             })
             .state('album', {
                 url: '/album',
-                controller: 'AlbumCtrl as album',
                 templateUrl: '/templates/album.html'
             })
             .state('collection', {
                 url: '/collection',
-                controller: 'CollectionCtrl as collection',
                 templateUrl: '/templates/collection.html'
             });
     };
