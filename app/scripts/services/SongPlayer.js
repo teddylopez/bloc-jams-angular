29195b133d5b4c44cdd100c1855458a63a042213
Lopez-Barnard <LopezE@advisory.com>
2017-03-12 18:12:59 -0400

    Added play and pause functionality

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,6 +1,62 @@
 (function() {
     function SongPlayer() {
         var SongPlayer = {};
+        var currentSong = null;
+ /**
+ * @desc Buzz object audio file
+ * @type {Object}
+ */
+        var currentBuzzObject = null;
+        
+/**
+* @function setSong
+* @desc Stops currently playing song and loads new audio file as currentBuzzObject
+* @param {Object} song
+*/
+        
+        var setSong = function(song) {
+            if (currentBuzzObject) {
+                currentBuzzObject.stop();
+                currentSong.playing = null;
+            }
+            currentBuzzObject = new buzz.sound(song.audioUrl, {
+                formats: ['mp3'],
+                preload: true
+            });
+            currentSong = song;
+        };
+
+/**
+* @function playSong
+* @desc Starts a song 
+* @param {Object} song
+*/
+        
+        playSong = function(song) {
+            currentBuzzObject.play();
+            song.playing = true;
+        };
+ 
+/**
+* @function used in ng-click
+* @desc ng-click function that switches between play and pause depending on whether the song is playing or not
+*/
+        SongPlayer.play = function(song) {
+            if(currentSong !== song) {
+            setSong(song);
+            playSong(song);
+            } else if (currentSong === song) {
+                if (currentBuzzObject.isPaused()) {
+                    currentBuzzObject.play();
+                }
+            }
+        };
+        
+        SongPlayer.pause = function(song) {
+            currentBuzzObject.pause();
+            song.playing = false;
+        };
+        
         return SongPlayer;
     }
  
