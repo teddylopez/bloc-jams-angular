981645212568dd12aa83b1c694ac6595aca47f7d
Lopez-Barnard <LopezE@advisory.com>
2017-03-12 14:14:06 -0400

    Created controllers for different views

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,15 +12,15 @@
                 controller: 'LandingCtrl as landing',
                 templateUrl: '/templates/landing.html'
             })
+           .state('collection', {
+                url: '/collection',
+                controller: 'CollectionCtrl as collection',
+                templateUrl: '/templates/collection.html'
+            })
             .state('album', {
                 url: '/album',
                 controller: 'AlbumCtrl as album',
                 templateUrl: '/templates/album.html'
-            })
-            .state('collection', {
-                url: '/collection',
-                controller: 'CollectionCtrl as collection',
-                templateUrl: '/templates/collection.html'
             });
     };
 
